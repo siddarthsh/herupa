9f0415ad469d8ef80f07cd4d9fb41c0a0ddc4073
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-05-29 19:36:33 +0530

    Version 0.1
    Built the following functions :-
    1) Users can Signup
    2) Users can create Posts
    3) Users can comment on Posts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# herupa
+# Herupa
+PWA built using ReactJS, NodeJS & MongoDB
+
+# Roadmap
+1) API (Still working on it)
+2) Frontend (Not started yet)
+
+# More
+This is an unfinished project, if you want to contribute, please first contact me "sayhi[at]siddarth.tech" and we'll discuss your role in the project.
---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+mongoose.Promise = global.Promise;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: [5, 'Username must be 5 characters or more.'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [8, 'Password must be 8 characters or more.'],
+  },
+  isDeleted:  {type:Boolean, default:false},
+  createdAt: {type:Date, default:Date.now},
+});
+
+// Write some encrption for Password
+
+const User = mongoose.model('User', userSchema);
+export default User;
