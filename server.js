0f56c138d10a7328608ad5d40c513023d28988fc
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-06-11 20:53:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ app.use("/api/users", users);
 // Serve static assets if we're in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("/client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 const port = process.env.PORT || 5000; // process.env.port helps set Heroku's port
