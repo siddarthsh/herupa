22519bcd59d490121700585a91ebf72b37eb1ac1
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-06-11 19:55:03 +0530

    Version 0.4.1

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('/../client/build'));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html' ));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));
     });
 }
 const port = process.env.PORT || 5000; // process.env.port helps set Heroku's port
