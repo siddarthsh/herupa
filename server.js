508a5ead42c5231ff129be2674def03b4bef5c78
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-06-11 19:04:40 +0530

    Version 0.1.3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/users");
 
@@ -32,7 +33,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+// Serve static assets if we're in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('/../client/build'));
 
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html' ));
+    });
+}
 const port = process.env.PORT || 5000; // process.env.port helps set Heroku's port
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
